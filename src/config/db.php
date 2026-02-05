4607c3e5b800e907c5696afafaae9a89f8c28a34
Deepaansh Deepak Sial <zatchwinston@gmail.com>
2026-02-06 00:03:31 +0530

    Update db.php

--- src/config/db.php
+++ src/config/db.php
@@ -1,4 +1,5 @@
 <?php
+	date_default_timezone_set('Asia/Kolkata');
 	$pdo = new PDO('mysql:host=localhost;dbname=introdb;charset=utf8','introdb_user','mypassword1!');
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
